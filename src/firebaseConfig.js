e6714dd812860dc8cd719bdf46a89c5de6ac4f55
Md.Mohtasim Shariar <onim04@gmail.com>
2021-03-21 16:37:42 +0600

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDKUMJc3YhWWWQiiai3p0MC4pQRidsmjU0",
+    authDomain: "solar-ticket.firebaseapp.com",
+    projectId: "solar-ticket",
+    storageBucket: "solar-ticket.appspot.com",
+    messagingSenderId: "967383125624",
+    appId: "1:967383125624:web:be75aa40a3ebb0b2f2278d",
+    measurementId: "G-89FESL5NSC"
+  };
+
+
+  export default firebaseConfig;
+  
